df02d7bedbb7ccd0787a46838d0930f01baab633
qianyong <qianyong1518@163.com>
2026-08-08 00:12:36 +0800

    fix: 文件选择器 accept 改为明确的图片 MIME 与扩展名列表，确保 SVG 可选

--- src/features/image-compressor/components/DropZone.tsx
+++ src/features/image-compressor/components/DropZone.tsx
@@ -39,7 +39,7 @@ export default function DropZone({ onFiles }: Props) {
       <input
         ref={inputRef}
         type="file"
-        accept="image/*,.svg"
+        accept="image/jpeg,image/png,image/webp,image/gif,image/bmp,image/svg+xml,image/avif,image/x-icon,image/tiff,image/vnd.microsoft.icon,.jpg,.jpeg,.png,.webp,.gif,.bmp,.svg,.avif,.ico,.tiff"
         multiple
         hidden
         onChange={(e) => {
